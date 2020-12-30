c075a8b994a3ebca0cafad4c506213ed8d75df82
Shiyan Boxer <shiyanboxer7@gmail.com>
2020-12-30 13:18:55 -0500

    create requests.py and work on def predict in app.py

--- Frontend/my-app/src/Forms.js
+++ Frontend/my-app/src/Forms.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
 import './App.css';
+import axios from "axios";
 
 class Forms extends Component {
+  // handelExtract(){
+  //   axios({
+  //       method: "POST",
+  //       url:"http://127.0.0.1:5000/predict"
+  //     }).then((response)=>{
+  //       if(response.data.is_error)
+  //       {
+  //           alert(response.data.error_message)
+  //           return
+  //       }
+  //       else
+  //       {
+  //           const data = response.data
+  //           console.log(data.all_data)
+  //           this.setState({displayPie:true, pieData:data.result_list, togglePie:!this.state.togglePie,tableData:data.all_data})
+  //       }
+  //     }).catch((error)=>{                                                
+  //       alert(error.message + " has occured")
+  //    }) 
+  // }
+
     render() {
         return (
         <form className="forms">
