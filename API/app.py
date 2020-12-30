ba7016c74fdd3058980b45318ed54cc146b88b12
Shiyan Boxer <shiyanboxer7@gmail.com>
2020-12-29 22:14:03 -0500

    add app.py

--- API/app.py
+++ API/app.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__)
 @app.route('/predict', methods=['GET'])
 def predict():
-    response = json.dumps({'response': 'yahhhh!'})
+    response = json.dumps({'response': 'hi there'})
     return response, 200
 
 if __name__ == '__main__':
