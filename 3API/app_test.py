59242b6bbb263def3837b3e6c2d58759210f75a3
Shiyan Boxer <shiyanboxer7@gmail.com>
2021-05-22 18:27:06 -0400

    write unit tests for hello and predict methods

--- 3API/app_test.py
+++ 3API/app_test.py
@@ -1,52 +1,85 @@
+import requests
 from app import app
 import flask.app as app
-import pytest
 import unittest
-import json
-from flask import Flask, jsonify
-import requests
+from flask import Flask
 
 # https://docs.python.org/3/library/unittest.html
 # https://www.youtube.com/watch?v=iQVvpnRYl-w
-# class TestCase(unittest.TestCase):
+# https://www.geeksforgeeks.org/get-post-requests-using-python/
 
 API_URL_HELLO = "http://127.0.0.1:5000/"
 API_URL_PREDICT = "http://127.0.0.1:5000/predict"
 class TestCase(unittest.TestCase):
-    # status code - 200 (ok)
+    # HELLO - status code - 200 (ok)
     def test_hello_status(self):
         response = requests.get(API_URL_HELLO)
         self.assertEqual(response.status_code, 200)
-        
-    # status code - 405 (method not allowed) without parameters
+    
+    # HELLO - checking request url
+    def test_url(self):
+        response = requests.get(API_URL_HELLO)
+        self.assertEqual(response.url, API_URL_HELLO)
+
+    # HELLO - request type
+    def test_hello_status(self):
+        response = requests.get(API_URL_HELLO)
+        self.assertTrue(str(response.request), "[GET]")
+
+    # HELLO - read contents type
+    def test_hello_readcontent(self):
+        response = requests.get(API_URL_HELLO)
+        self.assertEqual(response.headers["Content-Type"], "text/html; charset=utf-8")
+    
+    # HELLO - check for data returned
+    def test_read_content(self):
+        expectedResult = {"response": "hello world"}
+        response = requests.get(API_URL_HELLO).json()
+        self.assertEqual(response, expectedResult)
+    
+    # -------------------------------------------------
+
+    # PREDICT - status code - 200 (ok) with parameters
+    def test_predict_status200(self):
+        parameter = {"total_funding": 100, "founded_year": 100}
+        response = requests.get(url = API_URL_PREDICT, params = parameter)
+        self.assertEqual(response.status_code, 200)
+
+    # PREDICT - status code - 405 (method not allowed) without parameters
     def test_predict_status(self):
         response = requests.get(API_URL_PREDICT)
         self.assertEqual(response.status_code, 405)
-    
-    # status code - 200 (ok) with parameters
-    # def test_predict_status200(self):
-    #     parameter = {"total_funding": 100, "founded_year": 100}
-    #     response = requests.get(API_URL_PREDICT) + parameter
-    #     self.assertEqual(response.status_code, 200)
-    
-    # # read contents - type = (requests.models.Response)
-    # def test_hello_readcontent(self):
-    #     response = requests.get(API_URL_HELLO)
-    #     self.assertEqual(response.headers["Content-Type"], "application/json")
+      
+    # PREDICT - check status code if only one paramters is passed in
+    def test_predict_status(self):
+        parameter_1 = {"total_funding": 100}
+        parameter_2 = {"founded_year": 100}
+        response_1 = requests.get(url = API_URL_PREDICT, params = parameter_1)
+        response_2 = requests.get(url = API_URL_PREDICT, params = parameter_2)
+        self.assertEqual(response_1.status_code, 200)
+        self.assertEqual(response_2.status_code, 200)
 
-    # # read contents - type = (requests.models.Response)
-    # def test_hello_readcontent(self):
-    #     response = requests.get(API_URL_PREDICT)
-    #     self.assertEqual(response.headers["Content-Type"], "application/json")
+    # PREDICT - checking request url 
+    def test_url(self):
+        response = requests.get(API_URL_PREDICT)
+        self.assertEqual(response.url, API_URL_PREDICT)
 
-    # check for data returned
-    # def test_read_content(self):
-    #     response = requests.get(API_URL_PREDICT)
-    #     self.assertTrue(b'response' in response.data)
+    # PREDICT - read contents type
+    def test_hello_readcontent(self):
+        response = requests.get(API_URL_PREDICT)
+        self.assertEqual(response.headers["Content-Type"], "text/html; charset=utf-8")
     
-    # check response is between 0-1
-    # check status code if only one paramters is passed in
-    
+    # PREDICT - check for data returned
+    def test_read_content(self):
+        parameter = {"total_funding": 100, "founded_year": 100}
+        expectedResult = {"total_funding": 100, "founded_year": 100}
+        response = requests.get(url = API_URL_HELLO, params= parameter)
+        self.assertEqual(response.json(), expectedResult)
+
+    # PREDICT - check response is between 0-1
+    def test_respones_01(self):
+        response = requests.get(API_URL_PREDICT)
+        self.assertLessEqual(response.json(), 1)
 
 if __name__ == '__main__':
     unittest.main()
